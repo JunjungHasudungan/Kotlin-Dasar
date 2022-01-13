1f5c665db6a7dec62c639f52f716ef3a3566777d
JunjungHasudungan <junjunghasudungan43@gmail.com>
2022-01-13 13:08:59 +0700

    first commit

---
+++ src/main/kotlin/Variable.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    var namaLengkap:String = "Hasudungan"
+    var role:String = "Guru"
+    var umur:Int = 28
+    var pilihan:Boolean = false
+
+    println("Nama \t: " + namaLengkap)
+    println("Role \t: " + role)
+    println("Umur \t: " + umur)
+
+}
