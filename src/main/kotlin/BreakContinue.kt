1b6adfed904c2b6d5ea3a1b602ee2164f1690d2d
JunjungHasudungan <junjunghasudungan43@gmail.com>
2022-03-31 16:25:24 +0700

    second commit

---
+++ src/main/kotlin/BreakContinue.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    var index:Int = 1
+
+    while(index < 10)
+    {
+        if(index == 4)
+        {
+            break
+        }
+        println(index)
+        index++
+    }
+}
